5a684a73ac623daf1a0aaef73d373e8f709356a0
Maruf_Monem <marufmonem@gmail.com>
2022-09-18 12:54:02 +0600

    Added a new test case

---
+++ src/main/java/tests/video129_1.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class video129_1 {
+    @Test
+    public void demo(){
+        System.out.println("Hello");
+    }
+    @Test
+    public void demo2(){
+        System.out.println("World");
+    }
+}
