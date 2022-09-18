abfc21b65f3e9e9f83222c124656fcfc61b6bb0c
Maruf_Monem <marufmonem@gmail.com>
2022-09-18 13:35:30 +0600

    Adding more classes to learn how testing XML works

---
+++ src/main/java/tests/video130_1.java
@@ -0,0 +1,10 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class video130_1 {
+    @Test
+    public void demo(){
+        System.out.println("Hello from video 130");
+    }
+}
---
+++ src/main/java/tests/video131_1.java
@@ -0,0 +1,15 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class video131_1 {
+    @Test
+    public void webLogIn(){
+
+    }
+
+    @Test
+    public void mobileLogIn(){
+
+    }
+}
